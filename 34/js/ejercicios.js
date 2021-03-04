0d1a980802e7afbbb690b2baf2902ee6c9ff006c
Omar I. Martínez Gómez <omar_238@hotmail.com>
2021-03-04 17:38:49 -0600

    empezando 2da tanda de ejercicios

--- 34/js/ejercicios.js
+++ 34/js/ejercicios.js
@@ -31,10 +31,39 @@ let yo = {
 console.log(contarChar(yo))
 
 
+
 // 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
-function recortarStr(param) {
-    
+function recortarStr(param, noChars) {
+    if (typeof param === "string") {
+         return param.substring(0, noChars);
+    }
+    return 0;
 }
 
+console.log(recortarStr("Queridos reyes vagos", 10))
+
+
+
 // 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+function cadenaAArreglo(cadena, caracter) {
+    if (typeof cadena === "string") {
+        return cadena.split(' ')
+    }
+}
+
+console.log(cadenaAArreglo("Estos ejemplos son una pavada", ' '))
+
+
+
 // 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+function repiteStr(cadena, cantidad) {
+    let cad = ''
+    if (typeof cadena === "string") {
+        for (let i = 0; i < cantidad; i++) {
+            cad += `${cadena} `
+        }
+    }
+    return cad.trim()
+}
+
+console.log(repiteStr("Esto es un ejemplo extraño", 3))
