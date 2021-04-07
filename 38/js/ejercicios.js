ac41a9d9de1eb5e5c164574879975660ea9d9b90
Omar I. Martínez Gómez <omar_238@hotmail.com>
2021-04-06 23:31:10 -0500

    Ejercicio 1 de 3 completado

--- 38/js/ejercicios.js
+++ 38/js/ejercicios.js
@@ -6,9 +6,35 @@ const convertirBase = (numero = undefined, base = undefined) => {
     if(typeof base !== "number") return console.error("El parámetro \'base\' debe ser de tipo numérico.")
     if(numero % 1 !== 0) return console.error("El parámetro \'numero\' debe ser un número entero")
     if(base !== 2 && base !== 10) return console.error("El parámetro \'base\' sólo puede aceptar valores de 2 o 10")
+    if(base === 2 && numero.toString().search(/[2-9]/) !== -1) return console.error("El parámetro \'numero\' o no es un número binario válido o comienza con ceros")
 
+    let dividendo = numero,
+    cociente = 0,
+    residuo = 0,
+    i = 0
+    const divisor = 2
+    let binarioArr = []
     
+    if(base === 10) {
+        while (dividendo >= divisor) {
+            cociente = Math.trunc(dividendo / divisor)
+            residuo = dividendo % divisor
+            console.log(`${cociente} ${residuo}`)
+            dividendo = cociente
+            binarioArr[i++] = residuo
+        }
+        binarioArr[i] = dividendo
+
+        return console.info(`${binarioArr.reverse().join("")} base ${divisor}`)
+    } else {
+        return console.info(`${parseInt(numero.toString(), 2)} base 10`)
+    }
 }
+
+convertirBase(1000101110, 2)
+convertirBase(110000100100101, 2)
+
+
 
 // 16) Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad dada, pe. miFuncion(1000, 20) devolverá 800.
 
